4b498c47c3daac5ee151a929631891ff7d6c83a0
Haoran Xu <haoranxu510@gmail.com>
2022-06-26 23:23:40 -0700

    add a sanity test after importing json.hpp

---
+++ common_utils/json_utils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation-unknown-command"
+#pragma clang diagnostic ignored "-Wcomma"
+#pragma clang diagnostic ignored "-Wswitch-enum"
+#pragma clang diagnostic ignored "-Wtautological-overlap-compare"
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+
+#include "json.hpp"
+
+#pragma clang diagnostic pop
+
+using json = nlohmann::json;
+
---
+++ test_json_bytecode_loader.cpp
@@ -0,0 +1,23 @@
+#include "bytecode.h"
+#include "gtest/gtest.h"
+#include "json_utils.h"
+
+using namespace ToyLang;
+
+namespace {
+
+// Just make sure the json parser library works
+//
+TEST(JSONParser, Sanity)
+{
+    json j = json::parse("{ \"a\" : 1, \"b\" : \"cd\" }");
+    ReleaseAssert(j.is_object());
+    ReleaseAssert(j.count("a"));
+    ReleaseAssert(j["a"].is_primitive());
+    ReleaseAssert(j["a"] == 1);
+    ReleaseAssert(j.count("b"));
+    ReleaseAssert(j["b"].is_string());
+    ReleaseAssert(j["b"].get<std::string>() == "cd");
+}
+
+}   // anonymous namespace
