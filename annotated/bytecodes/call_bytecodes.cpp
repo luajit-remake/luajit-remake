a15908e31bd7b195ecd7127b7b04bafd71c5ab31
Haoran Xu <haoranxu510@gmail.com>
2022-10-10 19:19:57 -0700

    move tail call bytecode to a separate file, and add specializations

--- annotated/bytecodes/call_bytecodes.cpp
+++ annotated/bytecodes/call_bytecodes.cpp
@@ -53,41 +53,6 @@ void NO_RETURN CallOperationImpl(TValue* base, uint32_t numArgs, int32_t /*numRe
     }
 }
 
-template<bool passVariadicRes>
-void NO_RETURN TailCallOperationImpl(TValue* base, uint32_t numArgs)
-{
-    TValue func = base[0];
-    TValue* argStart = base + x_numSlotsForStackFrameHeader;
-    GetCallTargetConsideringMetatableResult callTarget = GetCallTargetConsideringMetatable(func);
-    if (callTarget.m_target.m_value == 0)
-    {
-        ThrowError(MakeErrorMessageForUnableToCall(func));
-    }
-
-    if (unlikely(callTarget.m_invokedThroughMetatable))
-    {
-        if constexpr(passVariadicRes)
-        {
-            MakeTailCallPassingVariadicRes(callTarget.m_target.As(), func, argStart, numArgs);
-        }
-        else
-        {
-            MakeTailCall(callTarget.m_target.As(), func, argStart, numArgs);
-        }
-    }
-    else
-    {
-        if constexpr(passVariadicRes)
-        {
-            MakeInPlaceTailCallPassingVariadicRes(argStart, numArgs);
-        }
-        else
-        {
-            MakeInPlaceTailCall(argStart, numArgs);
-        }
-    }
-}
-
 }   // anonymous namespace
 
 DEEGEN_DEFINE_BYTECODE_TEMPLATE(CallOperation, bool passVariadicRes)
@@ -114,20 +79,5 @@ DEEGEN_DEFINE_BYTECODE_TEMPLATE(CallOperation, bool passVariadicRes)
 
 DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(Call, CallOperation, false /*passVariadicRes*/);
 DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(CallM, CallOperation, true /*passVariadicRes*/);
-
-DEEGEN_DEFINE_BYTECODE_TEMPLATE(TailCallOperation, bool passVariadicRes)
-{
-    Operands(
-        BytecodeRangeBaseRW("base"),
-        Literal<uint32_t>("numArgs")
-    );
-    Result(NoOutput);
-    Implementation(TailCallOperationImpl<passVariadicRes>);
-    // TODO: support specialization
-    Variant();
-}
-
-DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(CallT, TailCallOperation, false /*passVariadicRes*/);
-DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(CallMT, TailCallOperation, true /*passVariadicRes*/);
 
 DEEGEN_END_BYTECODE_DEFINITIONS
---
+++ annotated/bytecodes/tail_call_bytecodes.cpp
@@ -0,0 +1,63 @@
+#include "bytecode_definition_utils.h"
+#include "deegen_api.h"
+
+#include "runtime_utils.h"
+
+namespace {
+
+template<bool passVariadicRes>
+void NO_RETURN TailCallOperationImpl(TValue* base, uint32_t numArgs)
+{
+    TValue func = base[0];
+    TValue* argStart = base + x_numSlotsForStackFrameHeader;
+    GetCallTargetConsideringMetatableResult callTarget = GetCallTargetConsideringMetatable(func);
+    if (callTarget.m_target.m_value == 0)
+    {
+        ThrowError(MakeErrorMessageForUnableToCall(func));
+    }
+
+    if (unlikely(callTarget.m_invokedThroughMetatable))
+    {
+        if constexpr(passVariadicRes)
+        {
+            MakeTailCallPassingVariadicRes(callTarget.m_target.As(), func, argStart, numArgs);
+        }
+        else
+        {
+            MakeTailCall(callTarget.m_target.As(), func, argStart, numArgs);
+        }
+    }
+    else
+    {
+        if constexpr(passVariadicRes)
+        {
+            MakeInPlaceTailCallPassingVariadicRes(argStart, numArgs);
+        }
+        else
+        {
+            MakeInPlaceTailCall(argStart, numArgs);
+        }
+    }
+}
+
+}   // anonymous namespace
+
+DEEGEN_DEFINE_BYTECODE_TEMPLATE(TailCallOperation, bool passVariadicRes)
+{
+    Operands(
+        BytecodeRangeBaseRW("base"),
+        Literal<uint32_t>("numArgs")
+    );
+    Result(NoOutput);
+    Implementation(TailCallOperationImpl<passVariadicRes>);
+    for (uint32_t numArgs = 0; numArgs < 6; numArgs++)
+    {
+        Variant(Op("numArgs").HasValue(numArgs));
+    }
+    Variant();
+}
+
+DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(CallT, TailCallOperation, false /*passVariadicRes*/);
+DEEGEN_DEFINE_BYTECODE_BY_TEMPLATE_INSTANTIATION(CallMT, TailCallOperation, true /*passVariadicRes*/);
+
+DEEGEN_END_BYTECODE_DEFINITIONS
