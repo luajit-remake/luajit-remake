40fb5ff4f1870b67661b23ae6782ad7ab69960b5
Haoran Xu <haoranxu510@gmail.com>
2022-11-07 22:12:08 -0800

    minor optimization to numeric for loop step

--- annotated/bytecodes/numeric_for_loop.cpp
+++ annotated/bytecodes/numeric_for_loop.cpp
@@ -33,6 +33,8 @@ static void NO_RETURN ForLoopInitImpl(TValue* base)
         }
     }
 
+    // Note that the 'vals[2]' may be NaN, so '!(vals[2] > 0)' does not imply 'vals[2] <= 0'
+    //
     bool loopConditionSatisfied = (vals[2] > 0 && vals[0] <= vals[1]) || (vals[2] <= 0 && vals[0] >= vals[1]);
     if (!loopConditionSatisfied)
     {
@@ -63,18 +65,46 @@ static void NO_RETURN ForLoopStepImpl(TValue* base)
     vals[2] = base[2].As<tDouble>();
 
     vals[0] += vals[2];
-    bool loopConditionSatisfied = (vals[2] > 0 && vals[0] <= vals[1]) || (vals[2] <= 0 && vals[0] >= vals[1]);
 
-    if (loopConditionSatisfied)
+    // Note that if vals[2] is NaN, the for-loop would have exited without any iteration.
+    // So having reached this bytecode, we know that vals[2] must not be NaN.
+    // Therefore, the 'vals[2] <= 0' term in the loop condition check
+    //     '(vals[2] > 0 && vals[0] <= vals[1]) || (vals[2] <= 0 && vals[0] >= vals[1])'
+    // can be optimized out.
+    //
+    assert(!IsNaN(vals[2]));
+
+    // Unfortunately we have to manually tail-duplicate the 'if', otherwise Clang would generate weird cmove code
+    // (that indeed has one less branch, but should perform inferior in this specific case since the 'vals[2] > 0'
+    // branch should be fairly predictable).
+    //
+    if (likely(vals[2] > 0))
     {
-        TValue v = TValue::Create<tDouble>(vals[0]);
-        base[0] = v;
-        base[3] = v;
-        ReturnAndBranch();
+        if (likely(vals[0] <= vals[1]))
+        {
+            TValue v = TValue::Create<tDouble>(vals[0]);
+            base[0] = v;
+            base[3] = v;
+            ReturnAndBranch();
+        }
+        else
+        {
+            Return();
+        }
     }
     else
     {
-        Return();
+        if (likely(vals[0] >= vals[1]))
+        {
+            TValue v = TValue::Create<tDouble>(vals[0]);
+            base[0] = v;
+            base[3] = v;
+            ReturnAndBranch();
+        }
+        else
+        {
+            Return();
+        }
     }
 }
 
